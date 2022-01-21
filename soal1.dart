9ec686de94ecb4b148f02c768dcb4e8ff772ca7f
unknown <ikevadilaike@gmail.com>
2022-01-21 13:55:17 -0800

    belajar dart

---
+++ soal1.dart
@@ -0,0 +1,10 @@
+void main() {
+  var word = 'Dart';
+  var second = ' is';
+  var third = ' awesome';
+  var fourth = ' and';
+  var fifth = ' I';
+  var sixth = ' love';
+
+  print(word + second + third + fourth + fifth + sixth);
+}
---
+++ soal3.dart
@@ -0,0 +1,12 @@
+import "dart:io";
+
+void main() {
+  //input
+  print("Masukan Nama Depan :");
+  String? depan = stdin.readLineSync();
+
+  print("Masukan Nama Belakang :");
+  String? belakang = stdin.readLineSync();
+
+  print("\nNama Lengkap Saya adalah : " + depan! + belakang!);
+}
